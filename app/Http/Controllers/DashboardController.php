82459bd8950694d6d52fb587975dcdb2145d4969
innotuy <innotuyi67@gmail.com>
2023-12-13 16:12:20 +0200

    adding views

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,6 +11,21 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        switch (auth()->user()->role) {
+            case 'instructor':
+              return  redirect()->route('instructor.dashboard');
+                break;
+            case 'member':
+              return  redirect()->route('member.dashboard');
+
+                break;
+            case 'admin':
+               return redirect()->route('admin.dashboard');
+                break;
+            default:
+            return redirect()->route('login');
+
+                break;
+        }
     }
 }
