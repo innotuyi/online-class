b983e1c48ab59a3be89269e5b56e70a4c7126a6d
innotuy <innotuyi67@gmail.com>
2023-12-21 16:02:23 +0200

    implementing seeder

---
+++ app/Http/Controllers/ScheduledClassController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClassType;
+use App\Models\ScheduledClass;
+use Illuminate\Http\Request;
+
+class ScheduledClassController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $scheduledClasses = auth()->user()->scheduledClasses()->oldest('date_time')->get();
+        return view('instructor.upcoming')->with('scheduledClasses', $scheduledClasses);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+
+        $classTypes = ClassType::all();
+        return view('instructor.schedule')->with('classTypes',$classTypes);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $date_time = $request->input('date') . " " . $request->input('time');
+
+        $request->merge([
+            'date_time' => $date_time,
+            'instructor_id' => auth()->user()->id
+        ]);
+
+        $validated = $request->validate([
+            'class_type_id' => 'required',
+            'instructor_id' => 'required',
+            'date_time' => 'required|unique:scheduled_classes,date_time|after:now'
+        ]);
+
+        ScheduledClass::create($validated);
+
+        return redirect()->route('schedule.index');
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
